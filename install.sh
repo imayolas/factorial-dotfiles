4f37c646f90aac549e56f4a8e528ec5b111b12b0
Isaac Mayolas <imayolas@gmail.com>
2022-08-11 10:34:00 +0200

    change approach to git config

--- install.sh
+++ install.sh
@@ -47,3 +47,6 @@ nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' # Yep, 
 # Install git alias
 cat ~/.gitconfig ./private_dot_config/git/gitconfig.extend > ~/.gitconfig
 git config --global push.default current
+git config --global alias.co commit
+git config --global alias.ch checkout
+git config --global alias.st status
